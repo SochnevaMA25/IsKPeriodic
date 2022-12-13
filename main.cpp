32164ead2e4f38615d81d1e13b8fbb74d183f3ff
SochnevaMA25 <115701819+SochnevaMA25@users.noreply.github.com>
2022-12-13 10:51:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
-//
-//  main.cpp
-//  IsKPeriodic
-//
-//  Created by Marina Sochneva on 10.12.2022.
-//
-
 #include <iostream>
 #include"Algoritm.h"
 
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "");
-    string txt;
-    cout << "Введите строку: " << endl;
-    cin >> txt;
-    int K; // проверка строки на кратность этому числу
-    cout << "Введите кратность: " << endl;
-    cin >> K;
-    Algoritm a(K, txt);
-    return 0;
+int main() 
+{
+	setlocale(LC_ALL, "");
+	string txt;
+	cout << "Введите строку: " << endl;
+	cin >> txt;
+	int len = txt.length();
+	int K; // проверка строки на кратность этому числу
+	cout << "Введите кратность: " << endl;
+	cin >> K;
+	Algoritm a(txt, len, K);
+	return 0;
 }
+
